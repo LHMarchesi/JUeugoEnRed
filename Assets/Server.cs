db0b67ef56ba29f0974757bea1caaecb9c1b9274
JuanMaHru <jhruby@uade.edu.ar>
2025-08-28 20:13:15 -0300

    entrar a sala y crearla

--- Assets/Server.cs
+++ Assets/Server.cs
@@ -1,25 +1,50 @@
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Diagnostics.Tracing;
+using TMPro;
 
 public class Server : MonoBehaviourPunCallbacks
 {
+    [SerializeField] TMP_InputField lobbyname;
+    [SerializeField] TMP_InputField code;
+    [SerializeField] Button getroom;
+    [SerializeField] Button createroom;
+
+
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
+        getroom.onClick.AddListener(EnterRoom);
+
     }
 
     public override void OnConnectedToMaster()
     {
-        CreateOrJoinRoomByName("Room0");
+        
     }
 
     public void CreateOrJoinRoomByName(string roomName)
     {
+        if (code != null)
+        {
+            PhotonNetwork.JoinRoom(roomName);
+        }
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.IsVisible = false;
         roomOptions.MaxPlayers = 4;
         PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+    }
+
+    public void EnterRoom()
+    {
+        CreateOrJoinRoomByName(code.name);
+    }
+    public void MakeRoom()
+    {
+        CreateOrJoinRoomByName(lobbyname.name);
     }
 
     public override void OnJoinedRoom()
