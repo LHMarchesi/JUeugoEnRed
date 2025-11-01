120f66f0c0801b919d74f7eacd32fafa05f10802
Lautaro Marchesi <159946877+LHMarchesi@users.noreply.github.com>
2025-11-01 19:41:38 -0300

    plataformaHodelder fix2

---
+++ Assets/_Scripts/Utilities/OnTriggerSetPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class OnTriggerSetPosition : MonoBehaviour
+{
+    public Transform objTransform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = objTransform.position;
+        other.transform.rotation = objTransform.rotation;
+        StartCoroutine(DesableCollisionsAndWait(0.5f));
+    }
+
+    IEnumerator DesableCollisionsAndWait(float time)
+    {
+        Collider col = GetComponent<Collider>();
+        col.enabled = false;
+        yield return new WaitForSeconds(time);
+        col.enabled = true;
+    }
+
+}
